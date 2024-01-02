7a75a7039b4ff5f20da41ba8179859aa57c15e62
Pavel <nntspector@icloud.com>
2024-01-02 12:17:48 +0300

    6,13 comp

--- ex 6,13/untitled/src/Животное.java
+++ ex 6,13/untitled/src/Животное.java
@@ -13,4 +13,19 @@ public class Животное {
         this.датаРождения = датаРождения;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getИмя() {
+        return имя;
+    }
+
+    public String getКоманда() {
+        return команда;
+    }
+
+    public Date getДатаРождения() {
+        return датаРождения;
+    }
 }
